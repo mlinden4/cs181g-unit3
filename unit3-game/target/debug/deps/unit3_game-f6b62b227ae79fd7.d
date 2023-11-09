92723b30a0246e6f77e34baeab0dcd1569ffc745
Max Linden <maxlinden@optimum.net>
2023-11-08 21:18:33 -0800

    created rust stuff

---
+++ unit3-game/target/debug/deps/unit3_game-f6b62b227ae79fd7.d
@@ -0,0 +1,5 @@
+/Users/maxlinden/Desktop/Tech/Rust/cs181g-unit3/unit3-game/target/debug/deps/unit3_game-f6b62b227ae79fd7.rmeta: src/main.rs
+
+/Users/maxlinden/Desktop/Tech/Rust/cs181g-unit3/unit3-game/target/debug/deps/unit3_game-f6b62b227ae79fd7.d: src/main.rs
+
+src/main.rs:
